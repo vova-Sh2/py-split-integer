58666358c89f087148dc2380bb8f6731cf68f98d
Vova <vovakucin082@gmail.com>
2025-11-26 10:25:47 +0100

    code write

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,3 +19,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(5, 10) == [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+
+def test_difference_between_the_max_and_min_result_is_not_more_than_one() -> None:
+    assert max(split_integer(32, 6)) - min(split_integer(32, 6)) <= 1
